328370eae8b6229c2863d1570974ec018b1421fd
Ciberusps <ciberus.ps@gmail.com>
2025-09-21 14:44:27 +0300

    EUHLSettingsSource move to UHLAI

--- Source/UnrealHelperLibrary/Public/UnrealHelperLibraryTypes.h
+++ Source/UnrealHelperLibrary/Public/UnrealHelperLibraryTypes.h
@@ -27,18 +27,6 @@ enum class EUHLDirection: uint8
     MAX UMETA(Hidden)
 };
 
-UENUM(BlueprintType)
-enum class EUHLSettingsSource: uint8
-{
-    None UMETA(Hidden),
-
-    Node,
-    DataAsset,
-    Actor,
-
-    MAX UMETA(Hidden)
-};
-
 // EUHLBuildType not same as EBuildConfiguration, if we WITH_EDITOR returns Editor,
 // no matter what EBuildConfiguration is currently used(mostly WITH_EDITOR is Development)
 // if real build returns EBuildConfiguration
